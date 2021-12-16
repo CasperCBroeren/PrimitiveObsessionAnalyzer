b7f268c0537eb47c8639ff9879300ec815456473
Casper Broeren <c.broeren@gmail.com>
2021-12-16 21:29:57 +0100

    Add project files.

---
+++ Analyzer/PrimitiveObsessionAnalyzer/PrimitiveObsessionAnalyzer.Test/PrimitiveObsessionAnalyzerUnitTests.cs
@@ -0,0 +1,99 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Threading.Tasks;
+using VerifyCS = PrimitiveObsessionAnalyzer.Test.CSharpCodeFixVerifier<
+    PrimitiveObsessionAnalyzer.PrimitiveObsessionAnalyzer,
+    PrimitiveObsessionAnalyzer.PrimitiveObsessionAnalyzerCodeFixProvider>;
+
+namespace PrimitiveObsessionAnalyzer.Test
+{
+    [TestClass]
+    public class PrimitiveObsessionAnalyzerUnitTest
+    {
+        //No diagnostics expected to show up
+        [TestMethod]
+        public async Task WhenNothingToAnalyze_Nothing()
+        {
+            var test = @"";
+
+            await VerifyCS.VerifyAnalyzerAsync(test);
+        }
+
+        //Diagnostic and CodeFix both triggered and checked for
+        [TestMethod]
+        public async Task WhenVariableOfString_ExpectAnalyzerToHit()
+        {
+            var test = @"
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using System.Diagnostics;
+
+    namespace ConsoleApplication1
+    {
+        public class Foo
+        {   
+            public Foo()
+            {
+                var name = ""AName"";
+            }
+        }
+    }"; 
+
+            var expected = VerifyCS.Diagnostic("PrimitiveObsessionAnalyzer").WithSpan(15, 17, 15, 36).WithArguments("name");
+            await VerifyCS.VerifyAnalyzerAsync(test, expected);
+        }
+
+        [TestMethod]
+        public async Task WhenPropertyOfInt_ExpectAnalyzerToHit()
+        {
+            var test = @"
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using System.Diagnostics;
+
+    namespace ConsoleApplication1
+    {
+        public class Foo
+        {   
+           public int Age { get; } = 12;
+        }
+    }";
+
+            var expected = VerifyCS.Diagnostic("PrimitiveObsessionAnalyzer").WithSpan(13, 19, 13, 22).WithArguments("Age");
+            await VerifyCS.VerifyAnalyzerAsync(test, expected);
+        }
+
+
+        [TestMethod]
+        public async Task WhenReturnTypeAndParameter_ExpectAnalyzerToHit()
+        {
+            var test = @"
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using System.Diagnostics;
+
+    namespace ConsoleApplication1
+    {
+        public class Foo
+        {   
+           public int AgeCalculator(int birthYear)
+            {
+                return DateTime.Now.Year - birthYear;
+            }
+        }
+    }";
+
+            var expectedMethod = VerifyCS.Diagnostic("PrimitiveObsessionAnalyzer").WithSpan(13, 19, 13, 22).WithArguments("AgeCalculator");
+            var expectedParameter = VerifyCS.Diagnostic("PrimitiveObsessionAnalyzer").WithSpan(13, 37, 13, 40).WithArguments("birthYear");
+            await VerifyCS.VerifyAnalyzerAsync(test, expectedMethod, expectedParameter);
+        }
+    }
+}
---
+++ Analyzer/PrimitiveObsessionAnalyzer/PrimitiveObsessionAnalyzer/PrimitiveObsessionAnalyzer.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Diagnostics;
+using System.Collections.Immutable;
+using System.Linq;
+
+namespace PrimitiveObsessionAnalyzer
+{
+    [DiagnosticAnalyzer(LanguageNames.CSharp)]
+    public class PrimitiveObsessionAnalyzer : DiagnosticAnalyzer
+    {
+        public const string DiagnosticId = "PrimitiveObsessionAnalyzer";
+
+        // You can change these strings in the Resources.resx file. If you do not want your analyzer to be localize-able, you can use regular strings for Title and MessageFormat.
+        // See https://github.com/dotnet/roslyn/blob/main/docs/analyzers/Localizing%20Analyzers.md for more on localization
+        private static readonly LocalizableString Title = new LocalizableResourceString(nameof(Resources.AnalyzerTitle), Resources.ResourceManager, typeof(Resources));
+        private static readonly LocalizableString MessageFormat = new LocalizableResourceString(nameof(Resources.AnalyzerMessageFormat), Resources.ResourceManager, typeof(Resources));
+        private static readonly LocalizableString Description = new LocalizableResourceString(nameof(Resources.AnalyzerDescription), Resources.ResourceManager, typeof(Resources));
+        private const string Category = "Declaration";
+
+        private static readonly DiagnosticDescriptor Rule = new DiagnosticDescriptor(DiagnosticId, Title, MessageFormat, Category, DiagnosticSeverity.Warning, isEnabledByDefault: true, description: Description);
+
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics { get { return ImmutableArray.Create(Rule); } }
+
+        public override void Initialize(AnalysisContext context)
+        {
+            context.ConfigureGeneratedCodeAnalysis(GeneratedCodeAnalysisFlags.None);
+            context.EnableConcurrentExecution();
+
+            context.RegisterSyntaxNodeAction(AnalyzeDeclaration, SyntaxKind.LocalDeclarationStatement);
+
+          //  context.RegisterSyntaxNodeAction(AnalyzeParameter, SyntaxKind.Parameter);
+
+            context.RegisterSyntaxNodeAction(AnlyzeReturnType, SyntaxKind.PredefinedType);
+        }
+        private void AnlyzeReturnType(SyntaxNodeAnalysisContext context)
+        {
+            var predefinedTypeDeclaration = (PredefinedTypeSyntax)context.Node;
+            var symbolInfo = context.SemanticModel.GetSymbolInfo(predefinedTypeDeclaration);
+            AnalyzeSymbol(symbolInfo, context, predefinedTypeDeclaration.GetLocation(), 
+                context.Node.Parent.DescendantTokens().OfType<SyntaxToken>().FirstOrDefault( x=> x.Kind() == SyntaxKind.IdentifierToken).Text);
+        }
+
+        private void AnalyzeDeclaration(SyntaxNodeAnalysisContext context)
+        {
+            var localDeclaration = (LocalDeclarationStatementSyntax)context.Node;
+            var symbolInfo = context.SemanticModel.GetSymbolInfo(localDeclaration.Declaration.Type);
+            AnalyzeSymbol(symbolInfo, context, localDeclaration.GetLocation(), context.Node.DescendantNodes().OfType<VariableDeclaratorSyntax>().First().Identifier.Text);
+        }
+
+        private void AnalyzeSymbol(SymbolInfo symbolInfo, SyntaxNodeAnalysisContext context, Location location, string declarator)
+        {
+            if (symbolInfo.Symbol != null && symbolInfo.Symbol.Name != "Void" && symbolInfo.Symbol.Name != "T")
+            {
+                if (symbolInfo.Symbol.Name == "String" || ((ITypeSymbol)symbolInfo.Symbol).BaseType.Name == "ValueType")
+                {
+                    var diagnostic = Diagnostic.Create(Rule, location, declarator);
+
+                    context.ReportDiagnostic(diagnostic);
+
+                }
+            }
+        }
+    }
+}
